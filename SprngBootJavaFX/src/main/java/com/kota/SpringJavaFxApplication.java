f03b19684f6cbd867ba232f23c395a392fe1bc85
Milan Kotarac <kotaracmilan@hotmail.com>
2020-09-29 21:57:32 +0200

    init

--- SprngBootJavaFX/src/main/java/com/kota/SpringJavaFxApplication.java
+++ SprngBootJavaFX/src/main/java/com/kota/SpringJavaFxApplication.java
@@ -1,15 +1,28 @@
 package com.kota;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import javafx.application.Application;
+import net.rgielen.fxweaver.core.FxWeaver;
+import net.rgielen.fxweaver.spring.SpringFxWeaver;
 
-@SpringBootApplication
+@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
+
 public class SpringJavaFxApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(DemoApplication.class, args);
 		Application.launch(JavaFxApplication.class, args);
+	}
+	
+	@Bean
+    public FxWeaver fxWeaver(ConfigurableApplicationContext applicationContext) {
+        // Would also work with javafx-weaver-core only:
+        // return new FxWeaver(applicationContext::getBean, applicationContext::close);
+        return new SpringFxWeaver(applicationContext); 
 	}
 
 }
